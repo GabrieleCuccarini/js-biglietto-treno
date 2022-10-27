920bbe390369ee175acb45b648a3cc6d04a071c6
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-10-27 15:52:37 +0200

    Completed

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 const kmDaPercorrere = prompt("Inserisci i chilometri da percorrere")
 const age = prompt("Inserisci la tua età")
 
+if ( isNaN( kmDaPercorrere )) {
+    alert( "Il numero inserito non è valido" );
+}
+
+
+if ( isNaN( age ))  {
+    alert( "Il numero inserito non è valido" );
+  }
+
 console.log(kmDaPercorrere, age)
+
+let prezzoDelBiglietto = kmDaPercorrere * 0.21
+
+console.log(prezzoDelBiglietto)
+
+if ( age < 18 ) {
+    let scontoMinorenni = prezzoDelBiglietto * 0.80 ;
+    const scontoMinorDeci = scontoMinorenni.toFixed(2)
+    console.log(scontoMinorDeci)
+  }
+
+if ( age > 64 ) {
+    let scontoOver = prezzoDelBiglietto * 0.60 ;
+    const scontoOverDeci = scontoOver.toFixed(2);
+    console.log(scontoOverDeci)
+}
+
